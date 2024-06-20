d4594e29f7bb59d3e76b1a544e98f21d4d72cf4b
haejin <megachj@gmail.com>
2024-06-20 19:30:24 +0900

    알고스팟

--- algospot/src/main/java/sunset/i1_설계패러다임/chap07_분할정복/problem/top_FANMEETING.java
+++ algospot/src/main/java/sunset/i1_설계패러다임/chap07_분할정복/problem/top_FANMEETING.java
@@ -1,12 +1,40 @@
 package sunset.i1_설계패러다임.chap07_분할정복.problem;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * https://www.algospot.com/judge/problem/read/FANMEETING
  *
  * 상태:
  */
 public class top_FANMEETING {
+
+    private static int C;
+    private static String[] MEMBERS;
+    private static String[] FANS;
+
     public static void main(String[] args) throws Exception {
-        // TODO
+        inputProblem();
+
+        for (int i = 0; i < FANS.length; ++i) {
+            solveUnitProblem(i);
+        }
+    }
+
+    private static void inputProblem() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        C = Integer.parseInt(br.readLine());
+        MEMBERS = new String[C];
+        FANS = new String[C];
+
+        for(int i = 0; i < C; ++i) {
+            MEMBERS[i] = br.readLine();
+            FANS[i] = br.readLine();
+        }
+    }
+
+    private static void solveUnitProblem(int c) throws Exception {
+        for()
     }
 }
