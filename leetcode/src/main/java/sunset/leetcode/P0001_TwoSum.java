de57473b29a21382ca527660a991b62fec775ceb
megachj <megachj@gmail.com>
2025-03-04 22:56:13 +0900

    leetcode: p0001

---
+++ leetcode/src/main/java/sunset/leetcode/P0001_TwoSum.java
@@ -0,0 +1,27 @@
+package sunset.leetcode;
+
+import java.util.Arrays;
+
+public class P0001_TwoSum {
+
+    public static void main(String[] args) {
+        Solution solution = new P0001_TwoSum().new Solution();
+        int[] result = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.length - 1; ++i) {
+                for (int j = i+1; j < nums.length; ++j) {
+                    if (nums[i] + nums[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
