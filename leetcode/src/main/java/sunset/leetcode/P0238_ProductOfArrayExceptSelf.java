4f6042faf21fea9c2ef3d110e0eb64c359f55459
megachj <megachj@gmail.com>
2025-03-30 15:54:50 +0900

    leetcode: P0238_ProductOfArrayExceptSelf 알고리즘 작성

--- leetcode/src/main/java/sunset/leetcode/P0238_ProductOfArrayExceptSelf.java
+++ leetcode/src/main/java/sunset/leetcode/P0238_ProductOfArrayExceptSelf.java
@@ -5,40 +5,33 @@ import java.util.Arrays;
 public class P0238_ProductOfArrayExceptSelf {
 
     public static void main(String[] args) {
+        int[] input1 = new int[]{-1, 1, 0, -3, 3, 0};
+        int[] input2 = new int[]{1, 2, 3, 4};
+        int[] input3 = new int[]{-1, 1, 0, -3, 3};
+
+        int[] input = input3;
         Solution solution = new P0238_ProductOfArrayExceptSelf().new Solution();
-        int[] input = new int[]{-1,1,0,-3,3, 0};
         int[] output = solution.productExceptSelf(input);
         System.out.println(Arrays.toString(output));
     }
 
     class Solution {
         public int[] productExceptSelf(int[] nums) {
-            int zeroCount = 0;
-            int totalProduct = 1;
-            int totalProductExceptZero = 1;
-            for (int i = 0; i < nums.length; ++i) {
-                totalProduct *= nums[i];
-                if (nums[i] == 0) {
-                    zeroCount++;
-                } else {
-                    totalProductExceptZero *= nums[i];
-                }
+            int[] result = new int[nums.length];
+
+            int product = 1;
+            result[0] = product;
+            for (int i = 1; i < nums.length; ++i) {
+                product = nums[i - 1] * product;
+                result[i] = product;
             }
 
-            int[] result = new int[nums.length];
-            for (int i = 0; i < nums.length; ++i) {
-                if (zeroCount >= 2) {
-                    result[i] = 0;
-                } else if (zeroCount == 1) {
-                    if (nums[i] == 0) {
-                        result[i] = totalProductExceptZero;
-                    } else {
-                        result[i] = 0;
-                    }
-                } else {
-                    result[i] = totalProduct / nums[i];
-                }
+            product = 1;
+            for (int i = nums.length - 2; i >= 0; --i) {
+                product = nums[i + 1] * product;
+                result[i] *= product;
             }
+
             return result;
         }
     }
