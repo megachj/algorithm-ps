f101afe8b8edba1a4dafc24c5d3984a0820e3c94
megachj <megachj@gmail.com>
2025-05-02 21:03:15 +0900

    leetcode: P0092_ReverseLinkedList2 해결

--- leetcode/src/main/java/sunset/leetcode/P0092_ReverseLinkedList2.java
+++ leetcode/src/main/java/sunset/leetcode/P0092_ReverseLinkedList2.java
@@ -4,8 +4,8 @@ public class P0092_ReverseLinkedList2 {
 
     public static void main(String[] args) {
         int[] inputList = new int[]{1, 2, 3, 4, 5};
-        int left = 2;
-        int right = 4;
+        int left = 1;
+        int right = 5;
 
         ListNode result = new P0092_ReverseLinkedList2().new Solution().reverseBetween(makeInput(inputList), left, right);
         printListNode(result);
@@ -13,9 +13,9 @@ public class P0092_ReverseLinkedList2 {
 
     /**
      * 설명<br>
-     * - 시간복잡도: <br>
-     * - 공간복잡도: <br>
-     * - 결과: ms / MB <br>
+     * - 시간복잡도: O(n)<br>
+     * - 공간복잡도: O(1)<br>
+     * - 결과: 0ms / 41.51MB <br>
      */
     class Solution {
         public ListNode reverseBetween(ListNode head, int left, int right) {
@@ -23,48 +23,46 @@ public class P0092_ReverseLinkedList2 {
                 return head;
             }
 
-            ListNode leftBeforePointer; // null 가능
+            ListNode preLeftPointer; // null 가능
             ListNode leftPointer;
             ListNode rightPointer;
-            ListNode rightAfterPointer; // null 가능
+            ListNode postRightPointer; // null 가능
 
-            // leftBefore 찾기
-            ListNode cur = null;
-            int position = 0;
-            while (position < left - 1) {
-                cur = cur == null ? head : cur.next;
-                position++;
+            ListNode pre, cur, post;
+            // head(1) 부터 시작
+            pre = null;
+            cur = head;
+            post = cur.next;
+
+            // left 까지 이동
+            for (int i = 1; i < left; ++i) {
+                pre = cur;
+                cur = post;
+                post = post.next;
             }
-            leftBeforePointer = cur;
+            preLeftPointer = pre;
+            leftPointer = cur;
 
-            // left 찾기
-            position++;
-            leftPointer = leftBeforePointer == null ? head : leftBeforePointer.next;
+            // right 까지 이동
+            for (int i = left; i < right; ++i) {
+                pre = cur;
+                cur = post;
+                post = post.next;
 
-            // left+1 부터 right 까지 순서 바꾸기
-            ListNode prev = leftPointer;
-            position++;
-            cur = leftPointer.next;
-            ListNode next = leftPointer.next;
-            for (; position <= right; ++position) {
-                cur.next = prev;
-                prev = cur;
-                cur = next;
-                next = next != null ? next.next : null;
+                cur.next = pre;
             }
-            // cur 가 right
             rightPointer = cur;
-            if (leftBeforePointer != null) {
-                leftBeforePointer.next = rightPointer;
-            }
-            // next 가 rightAfter
-            rightAfterPointer = next;
-            leftPointer.next = rightAfterPointer;
+            postRightPointer = post;
 
-            if (left == 1) {
-                return rightPointer;
-            } else {
+            if (preLeftPointer != null) {
+                preLeftPointer.next = rightPointer;
+            }
+            leftPointer.next = postRightPointer;
+
+            if (preLeftPointer != null) {
                 return head;
+            } else {
+                return rightPointer;
             }
         }
     }
