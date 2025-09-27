9fbbc226cd9a201c388d067c7a5fb4d083d5d60c
megachj <megachj@gmail.com>
2025-09-27 13:03:52 +0900

    leetcode: P0232 책 풀이 추가

--- leetcode/src/main/java/sunset/leetcode/P0232_ImplementQueueUsingStacks.java
+++ leetcode/src/main/java/sunset/leetcode/P0232_ImplementQueueUsingStacks.java
@@ -41,4 +41,40 @@ public class P0232_ImplementQueueUsingStacks {
             return stack.isEmpty();
         }
     }
+
+    /**
+     * 책 풀이법
+     */
+    class MyQueue2 {
+
+        private Deque<Integer> input;
+        private Deque<Integer> output;
+
+        public MyQueue2() {
+            this.input = new ArrayDeque<>();
+            this.output = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            input.push(x);
+        }
+
+        public int pop() {
+            peek();
+            return output.pop();
+        }
+
+        public int peek() {
+            if (output.isEmpty()) {
+                while (!input.isEmpty()) {
+                    output.push(input.pop());
+                }
+            }
+            return output.peek();
+        }
+
+        public boolean empty() {
+            return input.isEmpty() && output.isEmpty();
+        }
+    }
 }
