74fc816bfad209030e015e97d94f9740d7ca10b5
haejin <megachj@gmail.com>
2026-02-06 16:12:10 +0900

    leetcode: P0687 풀이

---
+++ leetcode/src/main/java/sunset/leetcode/P0687_LongestUnivaluePath.java
@@ -0,0 +1,44 @@
+package sunset.leetcode;
+
+import sunset.leetcode.support.datastructure.TreeNode;
+
+public class P0687_LongestUnivaluePath {
+
+    class Solution {
+        private int maxUnivaluePath;
+
+        public int longestUnivaluePath(TreeNode root) {
+            maxUnivaluePath = 0;
+
+            dfsNodeUnivaluePath(root);
+
+            return maxUnivaluePath;
+        }
+
+        private int[] dfsNodeUnivaluePath(TreeNode node) {
+            if (node == null) {
+                return new int[]{0, 0};
+            }
+
+            int[] leftChildUnivaluePath = dfsNodeUnivaluePath(node.left);
+            int[] rightChildUnivaluePath = dfsNodeUnivaluePath(node.right);
+
+            int[] nodeUnivaluePath = new int[]{0, 0};
+            if (node.left != null && node.val == node.left.val) {
+                nodeUnivaluePath[0] = Math.max(leftChildUnivaluePath[0], leftChildUnivaluePath[1]) + 1;
+            }
+            if (node.right != null && node.val == node.right.val) {
+                nodeUnivaluePath[1] = Math.max(rightChildUnivaluePath[0], rightChildUnivaluePath[1]) + 1;
+            }
+
+            if (node.left != null && node.right != null && node.left.val == node.right.val) {
+                maxUnivaluePath = Math.max(maxUnivaluePath, nodeUnivaluePath[0] + nodeUnivaluePath[1]);
+            } else {
+                maxUnivaluePath = Math.max(maxUnivaluePath, nodeUnivaluePath[0]);
+                maxUnivaluePath = Math.max(maxUnivaluePath, nodeUnivaluePath[1]);
+            }
+
+            return nodeUnivaluePath;
+        }
+    }
+}
