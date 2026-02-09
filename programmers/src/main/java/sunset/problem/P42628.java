b485f2d7450df9e2d92a58072da8a57b7e3f2d0d
haejin <megachj@gmail.com>
2026-02-09 18:06:46 +0900

    programmers: P42628 풀이

---
+++ programmers/src/main/java/sunset/problem/P42628.java
@@ -0,0 +1,69 @@
+package sunset.problem;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class P42628 {
+
+    private static final String[] INPUT_1 = new String[]{
+            "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"
+    };
+
+    private static final String[] INPUT_2 = new String[]{
+            "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"
+    };
+
+    public static void main(String[] args) {
+        String[] input = INPUT_2;
+
+        int[] result = new P42628().new Solution().solution(input);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+
+    /**
+     * 시간복잡도: O(nlogn)
+     * 공간복잡도: O(n)
+     * 결과: 73.51ms / 132MB
+     */
+    class Solution {
+
+        /**
+         * @param operations "I 숫자": 큐에 숫자 삽입, "D 1": 큐에서 최댓값 삭제, "D -1": 큐에서 최솟값 삭제
+         */
+        public int[] solution(String[] operations) {
+            TreeMap<Integer, Integer> map = new TreeMap<>(
+                    Comparator.naturalOrder()
+            );
+            for(String operation: operations) {
+                String[] split = operation.split(" ");
+                String op = split[0];
+                int n = Integer.parseInt(split[1]);
+
+                if ("I".equals(op)) {
+                    if (map.containsKey(n)) {
+                        map.put(n, map.get(n) + 1);
+                    } else {
+                        map.put(n, 1);
+                    }
+                } else {
+                    try {
+                        Map.Entry<Integer, Integer> entry = n == 1 ? map.lastEntry() : map.firstEntry();
+                        if (entry.getValue() == 1) {
+                            map.remove(entry.getKey());
+                        } else {
+                            map.put(entry.getKey(), entry.getValue() - 1);
+                        }
+                    } catch (Exception ignored) {}
+                }
+            }
+
+            if (map.isEmpty()) {
+                return new int[]{0, 0};
+            } else {
+                return new int[]{map.lastKey(), map.firstKey()};
+            }
+        }
+    }
+}
